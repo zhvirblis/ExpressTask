ba06fb3f6a0d7880ff60e9110281c204d9711faa
zhvirblis <zhvirblis@github.com>
2017-10-11 10:35:30 +0300

    Delete views

--- app.js
+++ app.js
@@ -25,10 +25,6 @@ app.use(webpackMiddleware(webpack(webpackConfig),{
   noInfo: false
 }));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -37,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/', index);
+/* app.use('/', index); */
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
