3dd5a2caa7965d3f51923f770412184b11cb5b84
zhvirblis <zhvirblis@github.com>
2017-10-11 08:40:02 +0300

    Red back, blue font

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractLess = new ExtractTextPlugin({
+    filename: "style.css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+    entry: './src/style.less',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '/dist')
+    },
+    module: {
+        rules: [{
+            test: /\.less$/,
+            use: extractLess.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "less-loader"
+                }],
+                fallback: "style-loader"
+            })
+        }]
+    },
+    plugins: [
+        extractLess
+    ]
+}
